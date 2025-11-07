fb835c96c5c38df415d92f0b1e4907e1c4195ae5
Carlos Dourado <carlosismaelad@outlook.com>
2025-11-07 18:02:17 -0300

    refactor: exchange localization for location

--- schemas/opening.go
+++ schemas/opening.go
@@ -1,6 +1,8 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -8,8 +10,21 @@ type Opening struct {
 	gorm.Model
 	Role string
 	Company string
-	Localization string
+	Location string
 	Remote bool
 	Link string
 	Salary int64
+}
+
+type OpeningResponse struct {
+	Id uint `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"upatedAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
+	Role string `json:"role"`
+	Company string `json:"company"`
+	Location string `json:"location"`
+	Remote bool `json:"remote"`
+	Link string `json:"link"`
+	Salary int64 `json:"salary"`
 }
